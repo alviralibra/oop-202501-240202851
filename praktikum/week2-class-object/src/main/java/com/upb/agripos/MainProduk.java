258a11c84ace97bbe340d04dbd66ebb43a7a41f9
Nama Kamu <email@example.com>
2025-10-08 13:13:24 +0700

    upload tugas week 2

--- praktikum/week2-class-object/src/main/java/com/upb/agripos/MainProduk.java
+++ praktikum/week2-class-object/src/main/java/com/upb/agripos/MainProduk.java
@@ -1,13 +1,14 @@
 package com.upb.agripos;
 
 import com.upb.agripos.model.Produk;
+import com.upb.agripos.util.CreditBy;
 
 public class MainProduk {
     public static void main(String[] args) {
 
-        Produk p1 = new Produk("ALR-010", "Benih Cabai AL10", 80000.0, 100);
+        Produk p1 = new Produk("ALR-002", "Benih Cabai AL10", 80000.0, 100);
         Produk p2 = new Produk("LBR-005", "Pupuk Organik 10kg", 90000.0, 100);
-        Produk p3 = new Produk("AVR-020", "Cangkul Kecil", 50000.0, 100);
+        Produk p3 = new Produk("AVR-025", "Cangkul Kecil", 50000.0, 100);
 
         System.out.println("=== Info Awal Produk ===");
         p1.tampilkanInfo();
@@ -15,13 +16,15 @@ public class MainProduk {
         p3.tampilkanInfo();
 
         System.out.println("\n=== Menambah Stok Produk ===");
-        System.out.println("Menambah stok Benih Cabai AL10 sebanyak 10");
-        p1.tambahStok(10);
+        System.out.println("Menambah stok Benih Cabai AL10 sebanyak 50");
+        p1.tambahStok(50);
         p1.tampilkanInfo();
 
         System.out.println("\n=== Mengurangi Stok Produk ===");
-        System.out.println("Mengurangi stok Cangkul Kecil sebanyak 5");
-        p3.kurangiStok(5);
+        System.out.println("Mengurangi stok Cangkul Kecil sebanyak 50");
+        p3.kurangiStok(50);
         p3.tampilkanInfo();
+
+        CreditBy.print();
     }
 }
