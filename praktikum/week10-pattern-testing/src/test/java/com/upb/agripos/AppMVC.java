1b563946f5d0f28210fe96f292edf207a63517f3
Nama Kamu <email@example.com>
2026-01-14 12:42:01 +0700

    praktikum week 10 pattern-testing

---
+++ praktikum/week10-pattern-testing/src/test/java/com/upb/agripos/AppMVC.java
@@ -0,0 +1,10 @@
+package com.upb.agripos;
+
+public class AppMVC {
+    public static void main(String[] args) {
+        System.out.println("Hello, Alvira Libra-240202851 (Week10)");
+        System.out.println("DatabaseConnection dibuat");
+        System.out.println("Terhubung ke database...");
+        System.out.println("Produk: P01 - Pupuk Organik");
+    }
+}
